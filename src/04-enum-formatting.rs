ef2086886af56709531aa0f9d254bb0926d69357
Leo Tytakoff <lev.taydakov@gmail.com>
2020-04-09 11:42:30 -0700

    Refactor 04

--- src/04-enum-formatting.rs
+++ src/04-enum-formatting.rs
@@ -1,3 +1,29 @@
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+#[structopt(name = "rustcli", about = "Rust CLI examples with StructOpt")]
+enum Opt {
+    #[structopt(about = "Create a new entity")]
+    Create {
+        title: String,
+        text: Option<String>
+    },
+    #[structopt(about = "Edit an existing entity")]
+    Edit {
+        id: i32,
+        title: Option<String>,
+        text: Option<String>
+    },
+    #[structopt(about = "List all entities")]
+    List
+}
+
 fn main() {
-    println!("03 Formatting Enumerations example");
+    let opt = Opt::from_args();
+    println!("opt = {:?}", opt);
+    match opt {
+        Opt::Create { title, text } => println!("Processing Opt::Create with title = {}, text = {:?}", title, text),
+        Opt::Edit { .. } => println!("Processing Opt::Edit with {:?}", opt),
+        Opt::List => println!("Processing Opt::List"),
+    }
 }
