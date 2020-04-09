c340a4e28a9bd16a8014e71522e1d4833b4d0241
Leo Tytakoff <lev.taydakov@gmail.com>
2020-04-09 10:52:14 -0700

    Implement 01-info-arg-names

---
+++ src/01-info-arg-names.rs
@@ -0,0 +1,19 @@
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+#[structopt(name = "rustcli", about = "Rust CLI examples with StructOpt")]
+struct Opt {
+    #[structopt(long)]
+    title: String,
+
+    #[structopt(short)]
+    cutoff: i32,
+
+    #[structopt(short, long)]
+    message: String
+}
+
+fn main() {
+    let opt = Opt::from_args();
+    println!("title = {:?}", opt);
+}
